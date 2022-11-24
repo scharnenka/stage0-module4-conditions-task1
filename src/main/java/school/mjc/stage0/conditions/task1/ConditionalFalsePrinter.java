0b8706d77ffccb8f49d4158b11ad25da1a320b14
Stas <chernenkosa@gmail.com>
2022-11-24 22:10:59 +0200

    task solution

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int i =1;
+        if (i<0) {
+            System.out.println(false);
+        } else {
+            System.out.println(false);
+        }
     }
 }
